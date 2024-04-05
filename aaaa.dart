a88938d5df0f2f3f8e647104780c331c942c21a1
waiphyosgx <137469347+waiphyosgx@users.noreply.github.com>
2024-04-05 13:15:52 +0800

    Update aaaa.dart

--- aaaa.dart
+++ aaaa.dart
@@ -1,5 +1,10 @@
- var ameSecurity =  await rootBundle.loadString("assets/js/ame_security_util.js");;
-    await _controller?.runJavaScriptReturningResult(ameSecurity);
-
-    final result = await _controller?.runJavaScriptReturningResult('encryptPinForAM("${ame[0]}","${ame[1]}","${ame[2]}","${ame[3]}","${ame[4]}")');
-    print('Result: $result'); // Output: Result: 5
+class AmeHelper {
+  encryptPinForAM(List args) async{
+    WebViewController myController = WebViewController();
+    myController.setJavaScriptMode(JavaScriptMode.unrestricted);
+    String ameSecurity =  await rootBundle.loadString("packages/sgx_online_login/assets/js/ame_security_util.js");
+    await myController.runJavaScript(ameSecurity);
+    final result = await myController.runJavaScriptReturningResult('encryptPinForAM("${args[0]}","${args[1]}","${args[2]}","${args[3]}","${args[4]}")');
+    return result;
+  }
+} 
