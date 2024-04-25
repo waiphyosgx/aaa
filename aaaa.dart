156abb1d22036795ae8803022a9e731f7340488c
waiphyosgx <137469347+waiphyosgx@users.noreply.github.com>
2024-04-25 14:25:26 +0800

    Update aaaa.dart

--- aaaa.dart
+++ aaaa.dart
@@ -1,23 +1,35 @@
-val binaryMessenger = flutterEngine?.dartExecutor?.binaryMessenger
-        if (binaryMessenger != null) {
-            methodChannel = MethodChannel(binaryMessenger, CHANNEL_NAME)
-            // Call Flutter after 10 seconds to get the value
-            Handler(Looper.getMainLooper()).postDelayed({
-                val key = "key1" // Change this to the desired key
-                methodChannel.invokeMethod("getValueFromFlutter", key, object : MethodChannel.Result {
-                    override fun success(result: Any?) {
-                        Toast.makeText(context, result?.toString(), Toast.LENGTH_SHORT).show()
-                    }
+   private suspend fun getStorageValue(key: String): String {
+        val binaryMessenger = flutterEngine?.dartExecutor?.binaryMessenger
+        var storageValue = ""
+        return suspendCoroutine {
+            if (binaryMessenger != null) {
+                methodChannel = MethodChannel(binaryMessenger, channel)
+                // Call Flutter after 10 seconds to get the value
+                methodChannel.invokeMethod(
+                    "getValueFromFlutter",
+                    key,
+                    object : MethodChannel.Result {
+                        override fun success(result: Any?) {
+                            storageValue = result.toString()
+                            Logger.d("result is getting $storageValue")
+                            it.resume(storageValue)
+                        }
 
-                    override fun error(errorCode: String, errorMessage: String?, errorDetails: Any?) {
-                        Toast.makeText(context, errorMessage?.toString(), Toast.LENGTH_SHORT).show()
-                    }
+                        override fun error(
+                            errorCode: String,
+                            errorMessage: String?,
+                            errorDetails: Any?
+                        ) {
+                            it.resume("")
+                        }
 
-                    override fun notImplemented() {
-                        Toast.makeText(context, "good", Toast.LENGTH_SHORT).show()
-                    }
-                })
-            }, 10000)
-        } else {
-            // Handle the case when binaryMessenger is null
+                        override fun notImplemented() {
+                            it.resume("")
+                        }
+                    })
+            }
+            else{
+                it.resume("")
+            }
         }
+    }
