ad7e47474c1aed86c828b46873d4262c9535894b
waiphyosgx <137469347+waiphyosgx@users.noreply.github.com>
2024-06-25 18:16:17 +0630

    Update aaaa.dart

--- aaaa.dart
+++ aaaa.dart
@@ -1,124 +1,73 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-part 'data_model.freezed.dart';
-part 'data_model.g.dart';
+class IFrameViewer extends StatefulWidget {
+  final String link;
 
-@freezed
-class DataModel with _$DataModel {
-  const factory DataModel({
-    Data? data,
-  }) = _DataModel;
+  const IFrameViewer({super.key, required this.link});
 
-  factory DataModel.fromJson(Map<String, dynamic> json) => _$DataModelFromJson(json);
+  @override
+  _IFrameViewerState createState() => _IFrameViewerState();
 }
 
-@freezed
-class Data with _$Data {
-  const factory Data({
-    Categories? categories,
-    AssetClassesSecurities? assetClassesSecurities,
-    Sectors? sectors,
-    ListData? list,
-  }) = _Data;
+class _IFrameViewerState extends State<IFrameViewer> {
+  late WebViewController _controller;
+  final _gestureRecognizers = <Factory<OneSequenceGestureRecognizer>>{
+    Factory<OneSequenceGestureRecognizer>(
+          () => EagerGestureRecognizer(),
+    ),
+  };
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController();
+    _controller.setJavaScriptMode(JavaScriptMode.unrestricted);
+    _controller.setNavigationDelegate(NavigationDelegate(
+      onPageFinished: (_)async{
+        final height = await _controller.runJavaScriptReturningResult('document.body.scrollHeight;');
+        print(height);
+      }
+    ));
+    _loadIframe();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: WebViewWidget(
+        controller: _controller,
+      ),
+    );
+  }
 
-  factory Data.fromJson(Map<String, dynamic> json) => _$DataFromJson(json);
-}
-
-@freezed
-class Categories with _$Categories {
-  const factory Categories({
-    List<CategoryData>? data,
-  }) = _Categories;
-
-  factory Categories.fromJson(Map<String, dynamic> json) => _$CategoriesFromJson(json);
-}
-
-@freezed
-class CategoryData with _$CategoryData {
-  const factory CategoryData({
-    Category? data,
-  }) = _CategoryData;
-
-  factory CategoryData.fromJson(Map<String, dynamic> json) => _$CategoryDataFromJson(json);
-}
-
-@freezed
-class Category with _$Category {
-  const factory Category({
-    String? id,
-    String? name,
-  }) = _Category;
-
-  factory Category.fromJson(Map<String, dynamic> json) => _$CategoryFromJson(json);
-}
-
-@freezed
-class AssetClassesSecurities with _$AssetClassesSecurities {
-  const factory AssetClassesSecurities({
-    List<AssetClassData>? data,
-  }) = _AssetClassesSecurities;
-
-  factory AssetClassesSecurities.fromJson(Map<String, dynamic> json) => _$AssetClassesSecuritiesFromJson(json);
-}
-
-@freezed
-class AssetClassData with _$AssetClassData {
-  const factory AssetClassData({
-    Category? data,
-  }) = _AssetClassData;
-
-  factory AssetClassData.fromJson(Map<String, dynamic> json) => _$AssetClassDataFromJson(json);
-}
-
-@freezed
-class Sectors with _$Sectors {
-  const factory Sectors({
-    List<AssetClassData>? data,
-  }) = _Sectors;
-
-  factory Sectors.fromJson(Map<String, dynamic> json) => _$SectorsFromJson(json);
-}
-
-@freezed
-class ListData with _$ListData {
-  const factory ListData({
-    int? count,
-    List<ListItem>? results,
-  }) = _ListData;
-
-  factory ListData.fromJson(Map<String, dynamic> json) => _$ListDataFromJson(json);
-}
-
-@freezed
-class ListItem with _$ListItem {
-  const factory ListItem({
-    ListItemData? data,
-  }) = _ListItem;
-
-  factory ListItem.fromJson(Map<String, dynamic> json) => _$ListItemFromJson(json);
-}
-
-@freezed
-class ListItemData with _$ListItemData {
-  const factory ListItemData({
-    String? contentType,
-    int? id,
-    String? title,
-    CategoryData? category,
-    List<AssetClassData>? assetClass,
-    Link? link,
-    String? migrated,
-    int? dateArticle,
-  }) = _ListItemData;
-
-  factory ListItemData.fromJson(Map<String, dynamic> json) => _$ListItemDataFromJson(json);
-}
-
-@freezed
-class Link with _$Link {
-  const factory Link({
-    String? url,
-  }) = _Link;
-
-  factory Link.fromJson(Map<String, dynamic> json) => _$LinkFromJson(json);
+  void _loadIframe() {
+    if (_controller != null) {
+      String html = '''
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Embedded Iframe</title>
+            <style>
+                .widget-security-details-information-iframe {
+                   display: block;
+                   width: 100%;
+                   height: 100vh;
+                    border: none;
+                    overflow: auto;
+                }
+            </style>
+        </head>
+        <body>
+            <iframe class="widget-security-details-information-iframe" src="${widget.link}"></iframe>
+        </body>
+        </html>
+      ''';
+      _controller.loadHtmlString(html);
+    }
+  }
 }
