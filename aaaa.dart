5ef37aa7e2f5081d06c16ba56f61714d08963fb0
waiphyosgx <137469347+waiphyosgx@users.noreply.github.com>
2024-07-17 16:40:20 +0630

    Update aaaa.dart

--- aaaa.dart
+++ aaaa.dart
@@ -1,66 +1,53 @@
-import 'dart:convert';
+ Map<String, dynamic> get queryParams {
+    final result = <String, dynamic>{};
 
-class RequestModel {
-  final int? limit;
-  final List<int>? assetClass;
-  final bool? assetClassFilterEnabled;
-  final List<int>? sector;
-  final bool? sectorFilterEnabled;
-  final int? fromDate;
-  final bool? fromDateFilterEnabled;
-  final int? toDate;
-  final bool? toDateFilterEnabled;
-  final int? category;
-  final bool? categoryFilterEnabled;
-  final String? securityCode;
-  final bool? securityCodeFilterEnabled;
-  final String? title;
-  final bool? titleFilterEnabled;
-  final String? lang;
+    if (categories != null && categories!.isNotEmpty) {
+      result['categories'] = categories!.join(',');
+    }
 
-  RequestModel({
-    this.limit,
-    this.assetClass,
-    this.assetClassFilterEnabled,
-    this.sector,
-    this.sectorFilterEnabled,
-    this.fromDate,
-    this.fromDateFilterEnabled,
-    this.toDate,
-    this.toDateFilterEnabled,
-    this.category,
-    this.categoryFilterEnabled,
-    this.securityCode,
-    this.securityCodeFilterEnabled,
-    this.title,
-    this.titleFilterEnabled,
-    this.lang,
-  });
+    if (!isBlank(searchType)) {
+      result['searchType'] = searchType;
+    }
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = {};
+    if (!isBlank(searchValue)) {
+      result['searchValue'] = searchValue;
+    }
 
-    if (limit != null) data['limit'] = limit;
-    if (assetClass != null) data['assetClass'] = assetClass;
-    if (assetClassFilterEnabled != null) data['assetClassFilterEnabled'] = assetClassFilterEnabled;
-    if (sector != null) data['sector'] = sector;
-    if (sectorFilterEnabled != null) data['sectorFilterEnabled'] = sectorFilterEnabled;
-    if (fromDate != null) data['fromDate'] = fromDate;
-    if (fromDateFilterEnabled != null) data['fromDateFilterEnabled'] = fromDateFilterEnabled;
-    if (toDate != null) data['toDate'] = toDate;
-    if (toDateFilterEnabled != null) data['toDateFilterEnabled'] = toDateFilterEnabled;
-    if (category != null) data['category'] = category;
-    if (categoryFilterEnabled != null) data['categoryFilterEnabled'] = categoryFilterEnabled;
-    if (securityCode != null) data['securityCode'] = securityCode;
-    if (securityCodeFilterEnabled != null) data['securityCodeFilterEnabled'] = securityCodeFilterEnabled;
-    if (title != null) data['title'] = title;
-    if (titleFilterEnabled != null) data['titleFilterEnabled'] = titleFilterEnabled;
-    if (lang != null) data['lang'] = lang;
+    if (!isBlank(from)) {
+      result['from'] = from;
+    }
 
-    return data;
+    if (!isBlank(to)) {
+      result['to'] = to;
+    }
+
+    if (subcategories != null && subcategories!.isNotEmpty) {
+      result['subcategories'] = subcategories!.join(',');
+    }
+
+    if (!isBlank(value)) {
+      result['value'] = value;
+    }
+
+    if (!isBlank(titleFilter)) {
+      result['titleFilter'] = titleFilter;
+    }
+
+    if (!isBlank(pageStart)) {
+      result['pagestart'] = pageStart;
+    }
+
+    if (!isBlank(pageSize)) {
+      result['pagesize'] = pageSize;
+    }
+
+    if (!isBlank(periodStart)) {
+      result['periodstart'] = periodStart;
+    }
+
+    if (!isBlank(periodEnd)) {
+      result['periodend'] = periodEnd;
+    }
+
+    return result;
   }
-
-  String toJsonString() {
-    return json.encode(toJson());
-  }
-}
