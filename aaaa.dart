bee6a9ac5e7cdfe03f423cbc820980e7f4089a1f
waiphyosgx <137469347+waiphyosgx@users.noreply.github.com>
2024-09-19 11:16:16 +0800

    Update aaaa.dart

--- aaaa.dart
+++ aaaa.dart
@@ -1,62 +1,122 @@
-<manifest xmlns:android="http://schemas.android.com/apk/res/android"
-    package="com.sgx.sgx_online">
-   <application
-        android:label="SGX Mobile"
-        android:name="${applicationName}"
-        android:icon="@mipmap/ic_launcher">
-        <activity
-            android:name=".MainActivity"
-            android:exported="true"
-            android:launchMode="singleTop"
-            android:theme="@style/LaunchTheme"
-            android:configChanges="orientation|keyboardHidden|keyboard|screenSize|smallestScreenSize|locale|layoutDirection|fontScale|screenLayout|density|uiMode"
-            android:hardwareAccelerated="true"
-            android:windowSoftInputMode="adjustResize">
-            <!-- Specifies an Android theme to apply to this Activity as soon as
-                 the Android process has started. This theme is visible to the user
-                 while the Flutter UI initializes. After that, this theme continues
-                 to determine the Window background behind the Flutter UI. -->
-            <meta-data
-              android:name="io.flutter.embedding.android.NormalTheme"
-              android:resource="@style/NormalTheme"
-              />
-            <intent-filter>
-                <action android:name="android.intent.action.MAIN"/>
-                <category android:name="android.intent.category.LAUNCHER"/>
-            </intent-filter>
-        </activity>
-        <!-- Don't delete the meta-data below.
-             This is used by the Flutter tool to generate GeneratedPluginRegistrant.java -->
-        <meta-data
-            android:name="flutterEmbedding"
-            android:value="2" />
-       <meta-data
-               android:name="com.facebook.sdk.ApplicationId"
-               android:value="@string/facebook_app_id" />
-       <meta-data
-               android:name="com.facebook.sdk.ClientToken"
-               android:value="@string/facebook_client_token" />
-       <activity
-               android:name="com.facebook.FacebookActivity"
-               android:configChanges="keyboard|keyboardHidden|screenLayout|screenSize|orientation"
-               android:label="@string/app_name" />
-       <activity
-               android:name="com.facebook.CustomTabActivity"
-               android:exported="true">
-           <intent-filter>
-               <action android:name="android.intent.action.VIEW" />
-               <category android:name="android.intent.category.DEFAULT" />
-               <category android:name="android.intent.category.BROWSABLE" />
-               <data android:scheme="@string/fb_login_protocol_scheme" />
-           </intent-filter>
-       </activity>
-       <activity android:name="com.linusu.flutter_web_auth_2.CallbackActivity" android:exported="true">
-           <intent-filter android:label="flutter_web_auth" android:autoVerify="true">
-               <action android:name="android.intent.action.VIEW" />
-               <category android:name="android.intent.category.DEFAULT" />
-               <category android:name="android.intent.category.BROWSABLE" />
-               <data android:scheme="${schemeName}" />
-           </intent-filter>
-       </activity>
-    </application>
-</manifest>
+import 'package:falcon_localizations/falcon_localizations.dart';
+import 'package:falcon_logger/falcon_logger.dart';
+
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:get_it/get_it.dart';
+
+import '../enums/content_loader_names.dart';
+import 'spinner_widget.dart';
+
+const _content = 'content';
+const _sharedContent = 'sharedContent';
+
+class FalconScreenWrapper extends ConsumerStatefulWidget {
+  const FalconScreenWrapper({
+    super.key,
+    required this.contentPath,
+    required this.factory,
+    this.sharedContentPath,
+    this.contentLoaderName,
+    this.routerState,
+    this.shimmer,
+  });
+
+  final String contentPath;
+  final String? sharedContentPath;
+  final String? contentLoaderName;
+
+  final GoRouterState? routerState;
+
+  final Widget? shimmer;
+
+  final Widget Function(
+    Map<String, dynamic> json, {
+    GoRouterState? routerState,
+    Map<String, dynamic>? sharedContentJson,
+  }) factory;
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _FalconScreenWrapperState();
+}
+
+class _FalconScreenWrapperState extends ConsumerState<FalconScreenWrapper> {
+  Map<String, dynamic>? contentJson;
+  Map<String, dynamic>? sharedContentJson;
+
+  @override
+  Widget build(BuildContext context) {
+    _registerListener();
+
+    if (contentJson == null) {
+      final falconLocale = ref.watch(GetIt.I.get<FalconLocale>());
+
+      return FutureBuilder<Map<String, Map<String, dynamic>?>>(
+        future: _loadContent(falconLocale.locale),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              contentJson = snapshot.data?[_content] ?? {};
+              sharedContentJson = snapshot.data?[_sharedContent];
+              return widget.factory(
+                contentJson!,
+                sharedContentJson: sharedContentJson,
+                routerState: widget.routerState,
+              );
+            } else if (snapshot.hasError) {
+              Log.e(snapshot.error?.toString() ??
+                  'Unknown Error For FalconContent!!!');
+            }
+          }
+
+          return widget.shimmer ?? const SpinnerWidget();
+        },
+      );
+    } else {
+      return widget.factory(
+        contentJson!,
+        sharedContentJson: sharedContentJson,
+        routerState: widget.routerState,
+      );
+    }
+  }
+
+  Future<Map<String, Map<String, dynamic>?>> _loadContent(Locale locale) async {
+    var content = _globalContent['${widget.contentPath}$locale'];
+    var sharedContent = _globalContent['${widget.sharedContentPath}$locale'];
+    if(content == null ||sharedContent == null) {
+      FalconContent falconContent = GetIt.I.get(
+        instanceName: widget.contentLoaderName ??
+            ContentLoaderNames.assetJsonContentLoder.name,
+      );
+
+      final List<Future<Map<String, dynamic>>> futures = [];
+      futures.add(falconContent.loadJson(widget.contentPath, locale));
+      if (widget.sharedContentPath != null) {
+        futures.add(falconContent.loadJson(widget.sharedContentPath!, locale));
+      }
+
+      final List<Map<String, dynamic>> data = await Future.wait(futures);
+      content = data.isNotEmpty ? data[0] : null;
+      sharedContent = data.length >= 2 ? data[1] : null;
+      _globalContent['${widget.contentPath}$locale'] = content ?? {};
+      _globalContent['${widget.sharedContentPath}$locale'] = sharedContent ?? {};
+    }
+    return {
+      _content: content,
+      _sharedContent: sharedContent,
+    };
+  }
+
+  _registerListener() {
+    ref.listen(GetIt.I.get<FalconLocale>(), (curr, next) {
+      setState(() {
+        // trigger rebuild to load new content
+      });
+    });
+  }
+}
+Map<String,Map<String,dynamic>> _globalContent = {};
+
