36842bc5f7997f307d3d492d5a7bb4cfde6c272a
waiphyosgx <137469347+waiphyosgx@users.noreply.github.com>
2024-11-21 17:32:11 +0800

    Update aaaa.dart

--- aaaa.dart
+++ aaaa.dart
@@ -1,165 +1,102 @@
-import 'dart:developer';
+import 'dart:convert';
 import 'dart:io';
-import 'package:flutter/foundation.dart';
-import 'package:xml/xml.dart';
+import 'package:csv/csv.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share/share.dart';
+import 'package:open_file/open_file.dart';
 
-import 'package:falcon_logger/falcon_logger.dart';
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
-import '../../../sgx_online_common_utils.dart';
-import '../native_utils/flutter_channel_invoker.dart';
-
-SessionUtils sessionUtils = SessionUtils();
-Future<void> migrateMobileStorage() async {
-  String uid = await sessionUtils.getDeviceToken();
-  String privateKey = await sessionUtils.getPrivateKey();
-  if (!kIsWeb && (uid.isEmpty || privateKey.isEmpty)) {
-    try {
-      String? privateKey;
-      //device id
-      String? deviceId;
-      //firebase token key
-      String? pushToken;
-      //user id for favourite social login
-      String? userId;
-      // String? push_token;
-
-      if (Platform.isAndroid) {
-        final oldSharedPrefPath = await _getNativeSharedPreferencesPath();
-        File file = File(oldSharedPrefPath);
-        if (file.existsSync()) {
-          String contents = await file.readAsString();
-          final document = XmlDocument.parse(contents);
-          Map<String, dynamic> nativePreferencesValue = {};
-          Iterable<XmlElement> elements = document.rootElement.children.whereType<XmlElement>();
-          for (XmlElement element in elements) {
-            String key = element.getAttribute('name') ?? '';
-            if(element.children.isNotEmpty && element.children.first.value != null) {
-               nativePreferencesValue[key] = element.children.first.value; // Store as string by default
-            }
-          }
-          privateKey = nativePreferencesValue['SYNC_PUB_KEY'];
-          deviceId = nativePreferencesValue['DEVICE_ID'];
-          // gcmKey = nativePreferencesValue['GCM'];
-          pushToken = nativePreferencesValue['TOKEN_PUSH'];
-          userId = nativePreferencesValue['UID'];
-          if(userId != null || userId?.isEmpty == null){
-            userId = deviceId;
-          }
-          print('private key is ${privateKey?.length} device id is $deviceId push token is $pushToken user id is $userId');
-          if (privateKey != null) {
-            await sessionUtils.savePrivateKey(privateKey: privateKey);
-          }
-          if (userId != null) {
-            sessionUtils.saveUUID(uuid: userId);
-          }
-          if (deviceId != null) {
-            await sessionUtils.saveDeviceToken(uuid: deviceId);
-          }
-          if (pushToken != null) {
-            await sessionUtils.savePushTokenKey(pushToken);
-          }
-        }
-      }
-    } catch (e) {
-      Log.e('migrateMobileStorage - error: $e');
-    }
-  }
-}
-
-Future<String> _getNativeSharedPreferencesPath() async {
+Future<void> exportToExcel(List<List<dynamic>> rows, String? fileName) async {
+  String csv = const ListToCsvConverter().convert(rows);
+  final bytes = utf8.encode(csv);
+  final directory = await getTemporaryDirectory();
+  final file = File('${directory.path}/${fileName ?? 'export'}.csv');
+  await file.writeAsBytes(bytes);
   if (Platform.isAndroid) {
-    Directory directory = Directory('/data/user/0/com.sgx.SGXandroid');
-    //android shared preference location
-    String sharedPreferenceLocation = '${directory.path}/shared_prefs/SGX_SHARED_PREFERENCES.xml';
-    return sharedPreferenceLocation;
-  }  else {
-    return throw Exception('unsupported platform');
+    _downloadForAndroid(bytes, '${fileName ?? 'export'}.csv', file.path);
+  }
+  else{
+    Share.shareFiles([file.path]);
   }
 }
 
-
-
-import 'dart:io';
-
-import 'package:falcon_logger/falcon_logger.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
-import 'package:get_it/get_it.dart';
-import 'package:go_router/go_router.dart';
-
-import '../../../sgx_online_common_services.dart';
-import '../../../sgx_online_common_utils.dart';
-import '../session_utils/migrate_mobile_storage.dart';
-
-class NativeChannelListener {
-  static const MethodChannel _tokenChannel = MethodChannel('$kAppBundleId/token');
-  static const MethodChannel _storageChannel = MethodChannel('$kAppBundleId/native_storage');
-
-  static Future<void> migrateOldApp() async{
-    GoRouter.optionURLReflectsImperativeAPIs = true;
-
-    //only for android and ios
-    try {
-      if (Platform.isIOS) {
-        _tokenChannel.setMethodCallHandler(_handleTokenMethod);
-        _storageChannel.setMethodCallHandler(_handleStorageMethod);
-      }
-      else if (Platform.isAndroid) {
-        migrateMobileStorage();
-      }
-      ScreenAwakeUtils.initScreenAwake();
+Future<void> _downloadForAndroid(
+    List<int> data, String fileName, String internalPath) async {
+  var status = await Permission.storage.request();
+  DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  final android = await deviceInfoPlugin.androidInfo;
+  if (status.isGranted || android.version.sdkInt >= 33) {
+    Directory? downloadDirectory = Directory('/storage/emulated/0/Download');
+    if (!await downloadDirectory.exists()) {
+      downloadDirectory = await getExternalStorageDirectory();
     }
-    catch(e){
-      //
+    if (downloadDirectory != null) {
+      String filePath = '${downloadDirectory.path}/$fileName';
+      File file = File(filePath);
+      await file.writeAsBytes(data);
+
+      // Show notification using NotificationManager
+      await _initializeNotifications(internalPath);
+      _showNotification(fileName, internalPath);
+    } else {
+      Share.shareFiles([internalPath]);
     }
-  }
-
-  static Future<void> _handleTokenMethod(MethodCall call) async {
-    switch (call.method) {
-      case 'saveToken':
-        _updatePushToken(call.arguments);
-        break;
-      default:
-        throw PlatformException(
-          code: 'Unimplemented',
-          details: 'The method ${call.method} is not implemented',
-        );
-    }
-  }
-
-  static Future<String?> _handleStorageMethod(MethodCall call) async {
-    try {
-      if (call.method == "storage") {
-        String key = call.arguments as String;
-        switch (key) {
-          case "privateKey":
-            return await _getSessionUtils().getPrivateKey();
-          case "devideId":
-            return await _getSessionUtils().getDeviceToken();
-          case "userId":
-            return await _getSessionUtils().getDeviceToken();
-          case "announcement":
-            return await _getSessionUtils().getAnnouncementToken();
-          default:
-            return "";
-        }
-      }
-      return "";
-    } catch (e) {
-      Log.e('_handleStorageMethod e - $e');
-      return "";
-    }
-  }
-
- static SessionUtils _getSessionUtils() {
-    return SessionUtils();
-  }
-
- static void _updatePushToken(String token) async {
-    UserSyncService userSyncService = GetIt.I.get();
-    await userSyncService.updatePushToken(token);
+  } else {
+    Share.shareFiles([internalPath]);
   }
 }
 
+Future<void> _initializeNotifications(String filePath) async {
+  const AndroidInitializationSettings androidInitializationSettings =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
 
+  const InitializationSettings initializationSettings =
+      InitializationSettings(android: androidInitializationSettings);
 
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+    onDidReceiveNotificationResponse: (NotificationResponse response) {
+      _handleNotificationClick(filePath);
+    },
+  );
+}
+
+Future<void> _showNotification(String fileName, String filePath) async {
+  const AndroidNotificationDetails androidNotificationDetails =
+      AndroidNotificationDetails(
+    'channel_id', // Unique channel ID
+    'channel_name', // Channel name
+    channelDescription: 'Channel description',
+    importance: Importance.high,
+    priority: Priority.high,
+  );
+
+  const NotificationDetails notificationDetails =
+      NotificationDetails(android: androidNotificationDetails);
+
+  await flutterLocalNotificationsPlugin.show(
+    0, // Notification ID
+    'File download successfully', // Notification Title
+    fileName, // Notification Body
+    payload: filePath, // Optional payload
+    notificationDetails,
+  );
+}
+
+void _handleNotificationClick(String payload) async {
+  OpenFile.open(
+    payload,
+    type: "text/plain",
+    linuxUseGio: true,
+    linuxByProcess: false,
+  ).catchError((e) {
+    //
+  }).whenComplete((){
+    Share.shareFiles([payload]);
+  });
+}
