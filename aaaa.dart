1a35c4d97f6862aeafe79ae891f7daa7d1298dec
waiphyosgx <137469347+waiphyosgx@users.noreply.github.com>
2024-11-29 17:29:21 +0800

    Update aaaa.dart

--- aaaa.dart
+++ aaaa.dart
@@ -1,201 +1,93 @@
-import UIKit
-import Flutter
-//import SwiftyJSON
+import 'package:falcon_logger/falcon_logger.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
+import 'package:go_router/go_router.dart';
 
-@UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-    
-    var secureManager: SecureManager?
+import '../../../sgx_online_common_services.dart';
+import '../../../sgx_online_common_utils.dart';
+import '../session_utils/migrate_mobile_storage.dart';
 
-    var loginRetry: Int = 0
-    var syncPublicKeyRetry: Int = 0
+class NativeChannelListener {
+  static const MethodChannel _tokenChannel = MethodChannel('$kAppBundleId/token');
+  static const MethodChannel _storageChannel = MethodChannel('$kAppBundleId/native_storage');
 
-    override func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        registerForPushNotifications()
+  static const MethodChannel _notificationChannel = MethodChannel('$kAppBundleId/notifications');
 
-        self.secureManager = SecureManager();
+  static Future<void> migrateSession() async {
+    GoRouter.optionURLReflectsImperativeAPIs = true;
 
-        GeneratedPluginRegistrant.register(with: self)
-        weak var registrar = self.registrar(forPlugin: "plugin-name")
+    //only for android and ios
+    try {
+      _tokenChannel.setMethodCallHandler(_handleTokenMethod);
+      _storageChannel.setMethodCallHandler(_handleStorageMethod);
+      await migrateMobileStorage();
+      ScreenAwakeUtils.initScreenAwake();
+    } catch (e) {
+      //
+    }
+  }
 
-//        let factory = FLNativeViewFactory(messenger: registrar!.messenger())
-//        self.registrar(forPlugin: "<plugin-name>")!.register(
-//            factory,
-//            withId: "nativeView")
+  static Future<void> _handleTokenMethod(MethodCall call) async {
+    print('called save token');
+    switch (call.method) {
+      case 'saveToken':
+        _updatePushToken(call.arguments);
+        break;
+      default:
+        throw PlatformException(
+          code: 'Unimplemented',
+          details: 'The method ${call.method} is not implemented',
+        );
+    }
+  }
 
-        guard let controller = window?.rootViewController as? FlutterViewController else {
-            return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  static Future<String?> _handleStorageMethod(MethodCall call) async {
+    try {
+      if (call.method == "storage") {
+        String key = call.arguments as String;
+        switch (key) {
+          case "privateKey":
+            return await _getSessionUtils().getPrivateKey();
+          case "devideId":
+            return await _getSessionUtils().getDeviceToken();
+          case "userId":
+            return await _getSessionUtils().getDeviceToken();
+          case "announcement":
+            return await _getSessionUtils().getAnnouncementToken();
+          default:
+            return "";
         }
-                
-        let storageChannel = FlutterMethodChannel(name: "com.sgx.SGX/native",
-                                                  binaryMessenger: controller.binaryMessenger)
-        storageChannel.setMethodCallHandler { (call, result) in
-            if call.method == "getIOSCredentials" {
-                guard let secureManager = self.secureManager else {
-                    print("secureManager is nil")
-                    result(FlutterError(code: "UNAVAILABLE", message: "SecureManager not initialized", details: nil))
-                    return
-                }
-                guard let privateKey = secureManager.exportCleanPrivateKeyToPKCS1() else {
-                    print("privateKey is nil")
-                    result(FlutterError(code: "UNAVAILABLE", message: "Private key not available", details: nil))
-                    return
-                }
-                guard let userId = secureManager.getUserId() else {
-                    print("userId is nil")
-                    result(FlutterError(code: "UNAVAILABLE", message: "User ID not available", details: nil))
-                    return
-                }
-                guard let deviceToken = secureManager.getDeviceToken() else {
-                    print("deviceToken is nil")
-                    result(FlutterError(code: "UNAVAILABLE", message: "Device token not available", details: nil))
-                    return
-                }
+      }
+      return "";
+    } catch (e) {
+      Log.e('_handleStorageMethod e - $e');
+      return "";
+    }
+  }
 
-                    let prefs = [
-                        "SYNC_PUB_KEY": privateKey,
-                        "DEVICE_ID": deviceToken,
-                        "USER_ID": userId,
-                        "PUSH_TOKEN": self.retrievePushToken() ?? "",
-                    ] as [String : Any]
-                    result(prefs)
-                result(FlutterMethodNotImplemented)
-            }  else if call.method == "getPushToken" {
-                let prefs = [
-                    "PUSH_TOKEN": self.retrievePushToken() ?? "",
-                ] as [String : Any]
-                result(prefs);
-            } else {
-                result(FlutterMethodNotImplemented)
-            }
+  static SessionUtils _getSessionUtils() {
+    return SessionUtils();
+  }
+
+  static void _updatePushToken(String token) async {
+    UserSyncService userSyncService = GetIt.I.get();
+    await userSyncService.updatePushToken(token);
+  }
+  static iosNotificationHandler({
+    required void Function(dynamic) onReceived,
+    required void Function(dynamic) onTapped,
+  }) {
+    _notificationChannel.setMethodCallHandler((call) async {
+      final dynamic payload = call.arguments;
+
+      if (payload != null) {
+
+        if (call.method == "onNotificationReceived") {
+          onReceived(payload);
+        } else if (call.method == "onNotificationTapped") {
+          onTapped(payload);
         }
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
-
-
-    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        let allowedBundleIDs: Set<String> = []
-           guard let sourceApp = options[.sourceApplication] as? String, allowedBundleIDs.contains(sourceApp) else {
-               print("Untrusted source application: \(String(describing: options[.sourceApplication]))")
-               return false
-           }
-           guard let scheme = url.scheme, scheme == "sgxmobile" else {
-               return false
-           }
-        return true
-    }
-
-    
-    class func showRequestError(_ message: String, title: String = "Error") {
-         // Ensure we have the correct AppDelegate instance
-         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-             return
-         }
-         
-         // Main thread check as UI updates must be on the main thread
-         DispatchQueue.main.async {
-             let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-             let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-             alertController.addAction(okAction)
-             
-             // Presenting on the rootViewController
-             if let rootVC = appDelegate.window?.rootViewController {
-                 // If the rootViewController is presenting another view controller, present the alert on that one
-                 var currentVC = rootVC
-                 while let presentedVC = currentVC.presentedViewController {
-                     currentVC = presentedVC
-                 }
-                 currentVC.present(alertController, animated: true, completion: nil)
-             }
-         }
-     }
-    
-    func registerForPushNotifications() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-            print("Permission granted: \(granted)")
-            guard granted else { return }
-            self.getNotificationSettings()
-        }
-    }
-
-    func getNotificationSettings() {
-        UNUserNotificationCenter.current().getNotificationSettings { settings in
-            guard settings.authorizationStatus == .authorized else { return }
-            DispatchQueue.main.async {
-                UIApplication.shared.registerForRemoteNotifications()
-            }
-        }
-    }
-
-    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        // Convert binary device token to a string
-        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
-        let token = tokenParts.joined()
-        print("Device Token: \(token)")
-        UserDefaults.standard.set(token, forKey: "pushNotificationToken")
-        UserDefaults.standard.synchronize() // This line is optional as UserDefaults automatically synchronizes data periodically.
-
-        // Send the token to Flutter
-//        sendPushTokenToFlutter(token)
-    }
-    
-    func updatePushToken() {
-        if let pushToken = UserDefaults.standard.object(forKey: "pushNotificationToken") as? String {
-            sendPushTokenToFlutter(pushToken)
-        }
-    }
-    
-    func retrievePushToken() -> String? {
-        return UserDefaults.standard.string(forKey: "pushNotificationToken")
-    }
-
-    override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        // Handle failure to receive push token
-        print("Failed to register: \(error)")
-    }
-
-    func sendPushTokenToFlutter(_ token: String) {
-        guard let controller = window?.rootViewController as? FlutterViewController else {
-            return
-        }
-        let channel = FlutterMethodChannel(name: "com.sgx.online/token", binaryMessenger: controller.binaryMessenger)
-        channel.invokeMethod("saveToken", arguments: token)
-    }
-    
-    override func applicationWillEnterForeground(_ application: UIApplication){
-        updatePushToken()
-    }
-    
-    override func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        tabNotification(payload: response.notification.request.content.userInfo)
-        
-        // Call the completion handler
-        completionHandler()
-    }
-    
-    override func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        // Handle the notification while the app is in the foreground
-        // For example, show an alert or play a sound
-        receividNotification(payload: notification.request.content.userInfo)
-        completionHandler([.alert, .sound, .badge])
-    }
-    // Send payload to Flutter
-        func receividNotification(payload: [AnyHashable: Any]) {
-            if let controller = window?.rootViewController as? FlutterViewController {
-                let channel = FlutterMethodChannel(name: "com.sgx.notifications", binaryMessenger: controller.binaryMessenger)
-                channel.invokeMethod("onNotificationReceived", arguments: payload)
-            }
-        }
-    
-    func tabNotification(payload: [AnyHashable: Any]) {
-        if let controller = window?.rootViewController as? FlutterViewController {
-            let channel = FlutterMethodChannel(name: "com.sgx.notifications", binaryMessenger: controller.binaryMessenger)
-            channel.invokeMethod("onNotificationTap", arguments: payload)
-        }
-    }
-
+      }
+    });
+  }
 }
-
