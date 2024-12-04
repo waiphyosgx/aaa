46264024a1c3c8934630024f68e114989d0b8ed8
waiphyosgx <137469347+waiphyosgx@users.noreply.github.com>
2024-12-04 15:46:49 +0800

    Update aaaa.dart

--- aaaa.dart
+++ aaaa.dart
@@ -1,315 +1,95 @@
-import 'dart:async';
-import 'dart:convert';
 
-import 'dart:io';
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    package="com.sgx.sgx_online">
+<queries>
+  <intent>
+    <action android:name="android.intent.action.VIEW" />
+    <data android:scheme="https" />
+  </intent>
+  <intent>
+    <action android:name="android.support.customtabs.action.CustomTabsService" />
+  </intent>
+  <intent>
+    <action android:name="android.intent.action.VIEW" />
+    <data android:scheme="mailto" />
+  </intent>
+</queries>
+ <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/>
+ <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>
+   <application
+        android:label="@string/app_name"
+        android:name="${applicationName}"
+        android:icon="@mipmap/ic_launcher">
+        <activity
+            android:name=".MainActivity"
+            android:exported="true"
+            android:launchMode="singleTop"
+            android:theme="@style/LaunchTheme"
+            android:configChanges="orientation|keyboardHidden|keyboard|screenSize|smallestScreenSize|locale|layoutDirection|fontScale|screenLayout|density|uiMode"
+            android:hardwareAccelerated="true"
+            android:windowSoftInputMode="adjustResize">
+            <!-- Specifies an Android theme to apply to this Activity as soon as
+                 the Android process has started. This theme is visible to the user
+                 while the Flutter UI initializes. After that, this theme continues
+                 to determine the Window background behind the Flutter UI. -->
+            <meta-data
+              android:name="io.flutter.embedding.android.NormalTheme"
+              android:resource="@style/NormalTheme"
+              />
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN"/>
+                <category android:name="android.intent.category.LAUNCHER"/>
+            </intent-filter>
+        </activity>
+       <receiver
+           android:name="com.huawei.hms.flutter.push.receiver.BackgroundMessageBroadcastReceiver"
+           android:exported="false">
+           <intent-filter>
+               <action android:name="com.huawei.hms.flutter.push.receiver.BACKGROUND_REMOTE_MESSAGE"/>
+           </intent-filter>
+       </receiver>
+        <meta-data
+            android:name="flutterEmbedding"
+            android:value="2" />
+       <meta-data
+           android:name="push_kit_auto_init_enabled"
+           android:value="true" />
 
-import 'package:falcon_logger/falcon_logger.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart' as google;
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:huawei_push/huawei_push.dart' as hw;
-import 'package:sgx_online_common/sgx_online_common_utils.dart';
-import 'package:sgx_online_common/src/utils/native_utils/price_list_cahce_utils.dart';
-import 'package:sgx_online_common/src/widgets/notifications/notification_popup.dart';
-
-Future<String?> getNotificationToken(GoRouter router) async {
-  //for huawei
-  if (Platform.isAndroid && await isHmsAvailable()) {
-    return Future(() async {
-      final Completer<String> completer = Completer<String>();
-      bool isGranted = await hw.Push.isAutoInitEnabled();
-      if (!isGranted) {
-        await hw.Push.setAutoInitEnabled(true);
-      }
-
-      hw.Push.getTokenStream.listen((token) {
-        if (!completer.isCompleted) {
-          completer.complete(token);
-        }
-      });
-      hw.Push.getToken('');
-
-      final complete = await completer.future;
-
-      return complete;
-    });
-  }
-  //for firebase (google android + apple)
-  else {
-    if (Firebase.apps.isEmpty) {
-      //double check for it
-      await Firebase.initializeApp();
-    }
-    google.FirebaseMessaging firebaseMessaging =
-        google.FirebaseMessaging.instance;
-    //request noti permission
-    await firebaseMessaging.requestPermission();
-    //get push token
-    if (Platform.isIOS) {
-      return await firebaseMessaging.getAPNSToken();
-    }
-    return await firebaseMessaging.getToken();
-  }
-}
-
-Future<void> registerNotification(GoRouter router) async {
-  //notification when app is not active
-  if (Platform.isAndroid && await isHmsAvailable()) {
-    await hw.Push.setAutoInitEnabled(true);
-   hw.Push.onMessageReceivedStream.listen(
-      (message) {
-        try {
-          final data = message.data;
-          final decodedData = jsonDecode(data ?? '{}');
-          final payloadList = decodedData['hcm'] as List;
-          Map<String, dynamic> payload = {};
-
-          for (var p in payloadList) {
-            payload.addAll(p);
-          }
-
-          if (payload.isNotEmpty) {
-            _showNotificationPopup(router, '', payload['message'], payload);
-          }
-        } catch (e) {
-          Log.e('getNotificationToken onMessageReceivedStream - $e');
-        }
-      },
-    );
-   final result = await  hw.Push.registerBackgroundMessageHandler(
-      registerHuaweiBackground,
-    );
-    hw.Push.onNotificationOpenedApp.listen((data) {
-      _onNotificationClick(data, router);
-    });
-  }
-  else {
-    google.FirebaseMessaging.onMessage.listen((data) {
-      final notification = data.notification;
-      _showNotificationPopup(router, notification?.title ?? '',
-          notification?.body ?? '', data.data);
-    });
-    google.FirebaseMessaging.onBackgroundMessage((data) async {
-      _onNotificationClick(data.data, router);
-    });
-    //notification when app is in foreground
-    google.FirebaseMessaging.onMessageOpenedApp.listen((data) {
-      _onNotificationClick(data.data, router);
-    });
-    if (Platform.isIOS) {
-      NativeChannelListener.iosNotificationHandler(
-        onReceived: (data) {
-          try {
-            final payload = data['aps'] as Map?;
-
-            final p = payload?.map((k, v) {
-              return MapEntry(k.toString(), v);
-            });
-
-            if (p?.isNotEmpty == true) {
-              _showNotificationPopup(router, '', p?['alert'] ?? '', p ?? {});
-            }
-          } catch (e) {
-            Log.e(
-                'apple notification received registerBackgroundMessageHandler - $e');
-          }
-        },
-        onTapped: (data) {
-          try {
-            final payload = data['aps'] as Map;
-
-            final p = payload.map((k, v) {
-              return MapEntry(k.toString(), v);
-            });
-            if (payload.isNotEmpty) {
-              _onNotificationClick(p, router);
-            }
-          } catch (e) {
-            Log.e(
-                'apple notification tab registerBackgroundMessageHandler - $e');
-          }
-        },
-      );
-    }
-  }
-}
-
-void registerHuaweiBackground(hw.RemoteMessage message) {
-  try {
-    final data = message.data;
-    final decodedData = jsonDecode(data ?? '{}');
-    final payloadList = decodedData['hcm'] as List;
-    Map<String, dynamic> payload = {};
-
-    // Iterate over the list and add all key-value pairs to the combined map
-    for (var p in payloadList) {
-      payload.addAll(p);
-    }
-
-    if (payload.isNotEmpty) {
-      hw.Push.localNotification(
-        <String, String>{
-          hw.HMSLocalNotificationAttr.TITLE: payload['title'] ?? '',
-          hw.HMSLocalNotificationAttr.MESSAGE: payload['message'] ?? '',
-        },
-      );
-    }
-  } catch (e) {
-    Log.e('getNotificationToken registerBackgroundMessageHandler - $e');
-  }
-}
-
-void _showNotificationPopup(GoRouter router, String title, String body,
-    Map<String, dynamic> data) async
-{
-  title = "SGX Notification";
-  String? type = data['type'];
-  Widget icon = const SizedBox.shrink();
-
-  if (type == 's' || type == 'i') {
-    Map<String, dynamic> securityCacheMap = await loadSecurityCache();
-    if (securityCacheMap.isEmpty) {
-      securityCacheMap = await fetchSecurityMap();
-    }
-    if (body.contains('decreased')) {
-      icon = Image.asset(
-        'assets/images/notifications/price_up.png',
-        package: 'sgx_online_common',
-        fit: BoxFit.fitWidth,
-        height: 16,
-      );
-    } else {
-      icon = Image.asset(
-        'assets/images/notifications/price_down.png',
-        package: 'sgx_online_common',
-        fit: BoxFit.fitWidth,
-        height: 16,
-      );
-    }
-  } else if (type == 'ipo') {
-    icon = Image.asset(
-      'assets/images/notifications/ic_ipo_outline.png',
-      package: 'sgx_online_common',
-      fit: BoxFit.fitWidth,
-      height: 16,
-    );
-  } else if (data['announcement_id'] != null) {
-    icon = Image.asset(
-      'assets/images/notifications/announcement.png',
-      package: 'sgx_online_common',
-      fit: BoxFit.fitWidth,
-      height: 16,
-    );
-  }
-  showSlideDownPopup(
-      context: router.routerDelegate.navigatorKey.currentContext!,
-      title: title,
-      message: body,
-      icon: SizedBox(
-        width: 16,
-        height: 16,
-        child: icon,
-      ),
-      data: data,
-      onClick: (payload) {
-        _onNotificationClick(payload, router);
-      });
-}
-
-void _onNotificationClick(Map<String, dynamic> data, GoRouter router) async {
-  final String? code = data['stock_code'];
-  final String? type = data['type'];
-  final String? announcementId = data['announcement_id'];
-  final String? url = data['url'];
-  // final String? title = data['title'];
-  String? ipoTitle = data['message'];
-  if (Platform.isIOS) {
-    ipoTitle = data['alert'];
-  }
-  if (type != null) {
-    switch (type) {
-      case 's': //security
-        {
-          if (code != null && code.isNotEmpty) {
-            _redirectToSecurity(router: router, code: code);
-          }
-        }
-        break;
-      case 'i': //indices
-        {
-          if (code != null && code.isNotEmpty) {
-            _redirectToIndices(router: router, code: code);
-          }
-        }
-      case 'ipo': //ipo notification
-        {
-          if (ipoTitle != null && url != null) {
-            router.pushNamed('upcoming-ipos-details', extra: {
-              'title': ipoTitle,
-              'url': url,
-            });
-          }
-        }
-        break;
-      default:
-        {
-          if (code != null) {
-            code.startsWith('.')
-                ? _redirectToSecurity(
-                    router: router,
-                    code: code.substring(1), //remove .
-                  )
-                : _redirectToIndices(router: router, code: code);
-          } else {
-            if (announcementId != null && announcementId.isNotEmpty) {
-              _redirectToAnnouncement(router: router, id: announcementId);
-            } else if (url != null && url.isNotEmpty) {
-              router.pushNamed('upcoming-ipos-details', extra: {
-                'title': ipoTitle,
-                'url': url,
-              });
-            }
-          }
-        }
-    }
-  }
-}
-
-void _redirectToSecurity(
-    {required GoRouter router, required String code}) async
-{
-  try {
-    Map<String, dynamic> securityCacheMap = await loadSecurityCache();
-    if (securityCacheMap.isEmpty) {
-      securityCacheMap = await fetchSecurityMap();
-    }
-    final type = securityCacheMap[code];
-    if (type != null) {
-      router.pushNamed(
-        'security-details',
-        pathParameters: {
-          'stockType': type.toString(),
-          'selectedStock': code,
-        },
-      );
-    } else {
-      Log.e('invalid security');
-    }
-  } catch (e) {
-    Log.e("can't redirect $e");
-  }
-}
-
-void _redirectToIndices({required GoRouter router, required String code}) {
-  router.pushNamed(
-    'indices-details',
-    pathParameters: {
-      'selectedStock': code,
-    },
-  );
-}
-
-void _redirectToAnnouncement({required GoRouter router, required String id}) {
-  router.pushNamed('/news/announcement-details/$id');
-}
+       <meta-data
+           android:name="com.huawei.hms.client.channel.androidMarket"
+           android:value="false" />
+       <meta-data
+               android:name="com.facebook.sdk.ApplicationId"
+               android:value="@string/facebook_app_id" />
+       <meta-data
+               android:name="com.facebook.sdk.ClientToken"
+               android:value="@string/facebook_client_token" />
+       <activity
+               android:name="com.facebook.FacebookActivity"
+               android:configChanges="keyboard|keyboardHidden|screenLayout|screenSize|orientation"
+               android:label="@string/app_name" />
+       <activity
+               android:name="com.facebook.CustomTabActivity"
+               android:exported="true">
+           <intent-filter>
+               <action android:name="android.intent.action.VIEW" />
+               <category android:name="android.intent.category.DEFAULT" />
+               <category android:name="android.intent.category.BROWSABLE" />
+               <data android:scheme="@string/fb_login_protocol_scheme" />
+           </intent-filter>
+       </activity>
+       <activity android:name="com.linusu.flutter_web_auth_2.CallbackActivity" android:exported="true">
+           <intent-filter android:label="flutter_web_auth" android:autoVerify="true">
+               <action android:name="android.intent.action.VIEW" />
+               <category android:name="android.intent.category.DEFAULT" />
+               <category android:name="android.intent.category.BROWSABLE" />
+               <data android:scheme="${schemeName}" />
+           </intent-filter>
+       </activity>
+    </application>
+    <queries>
+        <intent>
+            <action android:name="com.huawei.hms.core.aidlservice" />
+        </intent>
+    </queries>
+</manifest>
