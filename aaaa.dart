13bbd23930cabe2285956ae7b33a522f0c923ad4
waiphyosgx <137469347+waiphyosgx@users.noreply.github.com>
2024-12-10 13:11:49 +0800

    Update aaaa.dart

--- aaaa.dart
+++ aaaa.dart
@@ -1,33 +1,51 @@
-  static void registerDeepLink(GoRouter routers) {
-    appLink.uriLinkStream.listen((uri) {
-      String? category = uri.queryParameters['cat'];
-      String? type = uri.queryParameters['type'];
-      String? code = uri.queryParameters['code'];
-      if (category == "securities" &&
-          type?.isNotEmpty == true &&
-          code?.isNotEmpty == true) {
-        routers.pushNamed(
-          'security-details',
-          pathParameters: {
-            'stockType': type ?? '',
-            'selectedStock': code ?? '',
-          },
-        );
-      }
-      else if (category == "derivatives" && code?.isNotEmpty == true) {
-        routers.pushNamed('derivative-details',
-          pathParameters: {
-            'selectedStock': code ?? '',
-          },
-         );
-      }
-      else if(category == "indices" && code?.isNotEmpty == true){
-        routers.pushNamed(
-          'indices-details',
-          pathParameters: {
-            'selectedStock': code ?? '',
-          },
-        );
-      }
-    });
+import 'package:sgx_online_common/sgx_online_common_models.dart';
+import 'package:sgx_online_common/sgx_online_common_utils.dart';
+import 'package:share/share.dart';
+
+class ShareUtil {
+  static void shareText(String text) {
+    Share.share(text);
   }
+
+  static void shareTextWithSubject(String text, String subject) {
+    Share.share(text, subject: subject);
+  }
+
+  static void shareFiles(List<String> filePaths, String text) {
+    Share.shareFiles(filePaths, text: text);
+  }
+
+  static void sharePDF(String pdfPath, String text) {
+    Share.shareFiles([pdfPath], text: text);
+  }
+
+  static void shareLink(String url) {
+    Share.share(url);
+  }
+
+  static void shareSecurity(SecurityDataModel securityDataModel) {
+    if (securityDataModel.securityType?.code == null) {
+      return;
+    }
+    String shareURL =
+        '$shareSiteEndpoint?action=view&cat=securities&type=${securityDataModel.securityType!.code}&code=${securityDataModel.sgxCode}';
+    Share.share(shareURL);
+  }
+
+  static void shareDerivative(DerivativeData derivativeData) {
+    if (derivativeData.contractCode == null) {
+      return;
+    }
+    String shareURL =
+        '$shareSiteEndpoint?action=view&cat=derivatives&type=${derivativeData.derivativeType}&code=${derivativeData.contractCode}';
+    Share.share(shareURL);
+  }
+
+  static void shareIndice(IndiceData indiceData) {
+    if (indiceData.pid == null) {
+      return;
+    }
+    String shareURL = '$shareSiteEndpoint?action=view&cat=indices&code=${indiceData.pid}';
+    Share.share(shareURL);
+  }
+}
